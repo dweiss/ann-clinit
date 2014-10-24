5af8de10ff00498b0f34690a4462f539dbc80ea6
Dawid Weiss <dawid.weiss@carrotsearch.com>
2014-10-24 10:55:07 +0200

    Make the code valid.

--- Ann.java
+++ Ann.java
@@ -5,7 +5,7 @@ import static java.lang.annotation.RetentionPolicy.*;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Target({ FIELD, PARAMETER, METHOD })
+@Target({ FIELD, PARAMETER, METHOD, TYPE })
 @Retention(RUNTIME) 
 public @interface Ann {
   public static String FOO = new String("Foo");
